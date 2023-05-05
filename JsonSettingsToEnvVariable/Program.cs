07ed46058cdb4324fa98121f8e4117400f1d8b6f
lucasfmarconi <lucasfmarconi@gmail.com>
2023-05-05 10:27:20 +0200

    Configure logging

--- JsonSettingsToEnvVariable/Program.cs
+++ JsonSettingsToEnvVariable/Program.cs
@@ -1,4 +1,5 @@
 ﻿using JsonSettingsToEnvVariable.Converter;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,6 +13,8 @@ internal class Program
         try
         {
             var serviceProvider = BuildService(out var logger);
+
+            logger.LogInformation("Application starting...");
 
             var worker = serviceProvider.GetService<Worker>() ?? throw new ArgumentNullException(nameof(Worker));
 
@@ -58,16 +61,19 @@ internal class Program
 
     private static ServiceProvider BuildService(out ILogger<Program> logger)
     {
+        var configurationRoot = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
         var serviceCollection = new ServiceCollection()
             .AddLogging(c =>
             {
-                c.ClearProviders();
-                c.SetMinimumLevel(LogLevel.Information); //TODO: Change using args
                 c.AddSimpleConsole(options =>
                 {
                     options.SingleLine = true;
                     options.TimestampFormat = "(HH:mm:ss) ";
                 });
+                c.AddConfiguration(configurationRoot.GetSection("Logging"));
             });
 
         //Inject/Register dependencies
@@ -80,7 +86,8 @@ internal class Program
                   throw new ArgumentNullException(nameof(ILoggerFactory)))
             .CreateLogger<Program>();
 
-        logger.LogInformation("Application starting...");
+        logger.LogTrace("Services injected...");
+
         return serviceProvider;
     }
 }
